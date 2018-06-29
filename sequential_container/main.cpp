15e467c0cda4b78ab7700f6a5e02d7fb2d56def9
liujian <liuchanchen@live.com>
2018-06-29 17:11:35 +0800

    verified vector element address is continuously

--- sequential_container/main.cpp
+++ sequential_container/main.cpp
@@ -1,4 +1,5 @@
 #include "iterator.h"
+#include "verify_sequential_container.h"
 #include <iostream>
 
 using namespace std;
@@ -8,7 +9,8 @@ int main(int argc, char **argv)
     //find_elem_in_vector();
     //find_elem_in_list();
     //find_elem_in_move_vector();
-    swap_two_vector();
+    //swap_two_vector();
+    verify::verify_sequential_container();
 
     return 0;
 }
--- sequential_container/verify_sequential_container.cpp
+++ sequential_container/verify_sequential_container.cpp
@@ -1,14 +1,18 @@
 #include "verify_sequential_container.h"
+#include <cstdint>
 #include <iostream>
+#include <list>
+#include <vector>
 
 template <typename T, template <typename T1, typename _Allocator = std::allocator<T1>> class sequentail_container_type>
 class sequential_container_verification
 {
   private:
+    int elem_num;
     sequentail_container_type<T> templ_container;
 
   public:
-    sequential_container_verification(int);
+    sequential_container_verification(int, std::initializer_list<T>);
     //------------check sequentail container element location---------
     bool check_location();
     //------------check sequentail container access time cost---------
@@ -18,7 +22,13 @@ class sequential_container_verification
 };
 
 template <typename T, template <typename T1, typename _Allocator = std::allocator<T1>> class sequentail_container_type>
-sequential_container_verification<T, sequentail_container_type>::sequential_container_verification(int num){};
+sequential_container_verification<T, sequentail_container_type>::sequential_container_verification(int num, std::initializer_list<T> arg) : elem_num(num)
+{
+    for (auto elem : arg)
+    {
+        templ_container.push_back(elem);
+    }
+};
 
 template <typename T, template <typename T1, typename _Allocator = std::allocator<T1>> class sequentail_container_type>
 bool sequential_container_verification<T, sequentail_container_type>::check_location()
@@ -26,7 +36,7 @@ bool sequential_container_verification<T, sequentail_container_type>::check_loca
     int size = templ_container.size();
     auto iter = templ_container.begin();
 
-    std::cout << "---------print address------------" << std::endl;
+    std::cout << "---------print address----" << size << "--------" << std::endl;
     for (int i = 0; i < size; i++)
     {
         std::cout << "num:" << i << "address:" << &iter[i] << " " << std::endl;
@@ -49,5 +59,13 @@ bool sequential_container_verification<T, sequentail_container_type>::check_memb
 
 bool verify::verify_sequential_container()
 {
+    int32_t container_size = 8;
+    std::initializer_list<int32_t> arg = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    sequential_container_verification<int, std::vector> test_container_vec(container_size, arg);
+    test_container_vec.check_location();
+    sequential_container_verification<int, std::list> test_container_list(container_size, arg);
+    //test_container_list.check_location();
+
     return true;
 }
